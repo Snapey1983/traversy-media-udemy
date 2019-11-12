6aabec79bcb312c16855d70b3498bd30f6673606
Snapey1983 <david_snape@hotmail.co.uk>
2019-11-12 19:34:27 +0000

    console.log examples

---
+++ app.js
@@ -0,0 +1,38 @@
+// single line comment
+
+/*
+multi
+line 
+comment
+*/
+
+
+
+// Log to console
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+console.error('This is an error');
+console.clear();
+console.warn('This is a warning');
+
+console.time('Hello'); //time how long code takes to exicute
+console.log('Hello World'); //  / \
+console.log('Hello World'); //   |
+console.log('Hello World'); //   |
+console.log('Hello World'); //   |
+console.log('Hello World'); //   |
+console.log('Hello World'); //  \ /
+console.timeEnd('Hello'); //time how long code takes to exicute
+
+
+
+
+
+
