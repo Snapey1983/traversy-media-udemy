374179f8a357c5350b88eea683e4d0dbdf06ba00
Snapey1983 <david_snape@hotmail.co.uk>
2019-11-12 19:46:53 +0000

    var definition/explanation

--- app.js
+++ app.js
@@ -31,6 +31,22 @@ console.log('Hello World'); //   |
 console.log('Hello World'); //  \ /
 console.timeEnd('Hello'); //time how long code takes to exicute
 
+// var, let, const
+
+var name = 'John Doe';
+console.log(name);
+
+name = "Steve Smith"; // reasign variable or let... not const
+console.log(name);
+
+//init var - set to undefined
+var greeting;
+console.log(greeting);
+greeting = 'Hello'; // asign value to undefined var
+console.log(greeting);
+
+// variables can include - letters, numbers, _, $
+// cannot start with a number 
 
 
 
