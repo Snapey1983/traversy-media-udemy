bb78481d3ac2fb5924c4ac7bc21562754423b8d3
Snapey1983 <david_snape@hotmail.co.uk>
2019-11-15 15:26:18 +0000

    // WINDOW METHODS / OBJECTS / PROPERTIES

--- app.js
+++ app.js
@@ -687,12 +687,65 @@ const id = 100;
 // console.log(id);
 
 // FOR IN LOOP
-const user = {
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 40
-}
+// const user = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     age: 40
+// }
 
-for(let x in user){
-    console.log(`${x} : ${user[x]}`);
-}
+// for(let x in user){
+//     console.log(`${x} : ${user[x]}`);
+// }
+
+// WINDOW METHODS / OBJECTS / PROPERTIES
+
+// Alert
+// alert('Hello World');
+
+// Prompt
+// const input = prompt();
+// alert(input);
+
+//Confirm
+// if(confirm('Are you sure?')){
+//     console.log('YES');
+// } else {
+//     console.log('NO');
+// }
+
+let val;
+
+// Outer height and width
+val = window.outerHeight;
+val = window.outerWidth;
+
+// inner height and width
+val = window.innerHeight;
+val = window.innerWidth;
+
+// Scroll points
+val = window.scrollY;
+val = window.scrollX;
+
+// Location Object
+val = window.location
+val = window.location.hostname;
+val = window.location.port;
+val = window.location.href;
+val = window.location.search;
+
+// Redirect
+//window.location.href = 'https://google.com'
+
+// Reload
+//window.location.reload();
+
+// History Object
+//window.history.go(-9);
+//window.history.length;
+
+// Navigator Object
+val = window.navigator;
+val = window.navigator.appVersion;
+
+console.log(val);
