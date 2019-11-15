8e3389d8dd559e33d022a48eeaaa751bf2a4163b
Snapey1983 <david_snape@hotmail.co.uk>
2019-11-15 13:50:12 +0000

    Object leterals and if statements/comparison operators

--- app.js
+++ app.js
@@ -299,81 +299,243 @@ comment
 // html = `
 // <ul>
 //     <li>Name: $(name)</li>
-//     <li>Age: $(age)</li>
-//     <li>Job: $(job)</li>
-//     <li>City: $(city)</li>
-// </ul>`;
+// //     <li>Age: $(age)</li>
+// //     <li>Job: $(job)</li>
+// //     <li>City: $(city)</li>
+// // </ul>`;
 
-// document.body.innerHTML = html;
+// // document.body.innerHTML = html;
 
-// Arrays
+// // Arrays
 
-const numbers = [43,56,33,23,44,36,5];
-const numbers2 = new Array(22,45,33,76,54);
-const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
-const mixed = [22, 'hello', true, undefined, null, {a:1, b:1}, new Date()];
+// const numbers = [43,56,33,23,44,36,5];
+// const numbers2 = new Array(22,45,33,76,54);
+// const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
+// const mixed = [22, 'hello', true, undefined, null, {a:1, b:1}, new Date()];
 
-let val;
+// let val;
 
-// get array length
-val = numbers.length;
+// // get array length
+// val = numbers.length;
 
-// check if is array
-val = Array.isArray(numbers);
+// // check if is array
+// val = Array.isArray(numbers);
 
-// get single value
-val = numbers[3];
-val = numbers[0]; // arrays are zero based... 0,1,2,3, etc...
+// // get single value
+// val = numbers[3];
+// val = numbers[0]; // arrays are zero based... 0,1,2,3, etc...
 
-// insert into array
-numbers[2] = 100;
+// // insert into array
+// numbers[2] = 100;
 
-//find index of value
-val = numbers.indexOf(36);
+// //find index of value
+// val = numbers.indexOf(36);
 
-// MUTATING ARRAYS
-// // add on to end
-// numbers.push(250);
+// // MUTATING ARRAYS
+// // // add on to end
+// // numbers.push(250);
 
-// // add on to front
-// numbers.unshift(120);
+// // // add on to front
+// // numbers.unshift(120);
 
-// //take off from end
-// numbers.pop();
+// // //take off from end
+// // numbers.pop();
 
-// //take off from front
-// numbers.shift();
+// // //take off from front
+// // numbers.shift();
 
-// // splice values
-// numbers.splice(1,3);
+// // // splice values
+// // numbers.splice(1,3);
 
-// // reverse
-// numbers.reverse();
+// // // reverse
+// // numbers.reverse();
 
-// // concatenate array
-// val = numbers.concat(numbers2);
+// // // concatenate array
+// // val = numbers.concat(numbers2);
 
-// sorting array
-// val = fruit.sort();
-// val = numbers.sort();
+// // sorting array
+// // val = fruit.sort();
+// // val = numbers.sort();
 
-// // use the "compare" function
-// val = numbers.sort(function(x, y){
-//     return x - y;
-// })
+// // // use the "compare" function
+// // val = numbers.sort(function(x, y){
+// //     return x - y;
+// // })
 
-// // reverse sort
-// val = numbers.sort(function(x, y){
-//     return y - x;
-// })
+// // // reverse sort
+// // val = numbers.sort(function(x, y){
+// //     return y - x;
+// // })
 
-// find
-function under50(num){
-    return num < 50;
+// // find
+// function under50(num){
+//     return num < 50;
+// }
+
+// val = numbers.find(under50);
+
+// console.log(numbers);
+// console.log(val);
+// console.log(fruit);
+
+
+// OBJECT LITERALS
+
+// const person = {
+//     firstname: 'Steve',
+//     lastName: 'Smith',
+//     age: 36,
+//     email: 'steve@aol.com',
+//     hobbies: ['music', 'sports'],
+//     address: {
+//         city: 'Miami',
+//         state: 'FL'
+//     },
+//     getBirthYear: function(){
+//         return 2017 - this.age;
+//     }
+// }
+
+// let val;
+
+// val = person;
+// //get specific value
+// val = person.firstName;
+// val = person['lastName'];
+// val = person.age;
+// val = person.hobbies[1];
+// val = person.address.state;
+// val = person.address['city'];
+// val = person.getBirthYear();
+
+// console.log(val);
+
+// const people = [
+//     {name: 'John', age: 30},
+//     {name: 'Mike', age: 23}
+// ];
+
+// for(let i = 0; i < people.length; i++){
+//     console.log(people[i].name)
+// }
+
+// DATE OBJECT
+
+// let val;
+
+// const today = new Date();
+// let birthday = new Date('9-10-1981 11:25:00');
+// birthday = new Date('September 10 1981');
+
+// val = today.getMonth();
+// val = today.getDate();
+// val = today.getDay();
+// val = today.getFullYear();
+// val = today.getHours();
+// val = today.getMinutes();
+// val = today.getSeconds();
+// val = today.getMilliseconds();
+// val = today.getTime();
+
+// birthday.setMonth(2);
+// birthday.setDate(12);
+// birthday.setFullYear(1985);
+// birthday.setHours(2);
+// birthday.setMinutes(2);
+// birthday.setSeconds(2);
+
+
+// console.log(birthday);
+
+// IF STATEMENTS AND COMPARISON OPERATORS
+
+// if(something){
+//     do something
+// } else {
+//     do something else
+// }
+
+const id = 100;
+
+// // EQUAL TO
+
+// if(id == 100){
+//     console.log('CORRECT');
+// } else {
+//     console.log('INCORRECT');
+// }
+
+// // NOT EQUAL TO
+
+// if(id != 101){
+//     console.log('CORRECT');
+// } else {
+//     console.log('INCORRECT');
+// }
+
+// // EQUAL TO VALUE AND TYPE
+
+// if(id === 100){
+//     console.log('CORRECT');
+// } else {
+//     console.log('INCORRECT');
+// }
+
+// // NOT EQUAL TO VALUE AND TYPE
+
+// if(id !== 100){
+//     console.log('CORRECT');
+// } else {
+//     console.log('INCORRECT');
+// }
+
+// Test if undefined
+// if(typeof id !== 'undefined'){
+//     console.log(`The ID is ${id}`);
+// } else {
+//     console.log('No ID');
+// }
+
+// GREATER OR LESS THAN >, >=, ==, <=, <
+if(id > 200){
+    console.log('CORRECT');
+} else {
+    console.log('INCORRECT');
 }
 
-val = numbers.find(under50);
+// IF ELSE
 
-console.log(numbers);
-console.log(val);
-console.log(fruit);
+const color = 'red';
+
+if (color === 'red'){
+   console.log('color is red'); 
+} else if(color === 'blue'){
+    console.log('color is blue');
+} else {
+    console.log('color is not red or blue');
+}
+
+// LOGICAL OPERATORS
+
+const name = 'Steve';
+const age = 20;
+
+// AND &&
+if(age > 0 && age < 12){
+    console.log(`${name} is a child`);
+} else if(age >= 13 && age <= 19){
+    console.log(`${name} is a teenager`);
+} else {
+    console.log(`${name} is an adult`)
+}
+
+// OR ||
+
+if(age < 16 || age > 65){
+    console.log(`${name} can not run in race`);
+} else {
+    console.log(`${name} is registered for the race`)
+}
+
+// TERNARY OPERATOR
+console.log(id === 100 ? 'CORRECT' : 'INCORRECT')
