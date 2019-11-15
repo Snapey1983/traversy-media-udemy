7070d4e23076e4d64a2eb2cee1ebcad70a93228e
Snapey1983 <david_snape@hotmail.co.uk>
2019-11-15 14:54:59 +0000

    For In While Do Loops

--- app.js
+++ app.js
@@ -625,3 +625,74 @@ const id = 100;
 // todo.add();
 // todo.edit(22);
 // todo.delete();
+
+// GENERAL LOOPS
+
+// FOR LOOP
+
+// for (let i = 0; i < 10; i++ ){
+//     //console.log('Number ' + i);
+//     if (i === 2){
+//         console.log('2 is my favorite number');
+//         continue;
+//     }
+
+//     if (i===5){
+//         console.log('stop the loop')
+//         break;
+//     }
+
+//     console.log('Number ' + i);
+// }
+
+// WHILE LOOP
+
+// let i = 0;
+
+// while(i < 10){
+//     console.log('Numper ' + i);
+//     i++;
+// }
+
+// DO WHILE LOOP
+
+// let i = 0;
+
+// do {
+//     console.log('Numper ' + i);
+//     i++;
+// }
+
+// while(i < 10);
+
+
+// LOOP THROUGH ARRAY
+// const cars = ['Ford', 'Chevy', 'Honda', 'Toyota'];
+
+// // for(let i = 0; i < cars.length; i++){
+// //     console.log(cars[i]);
+// // }
+
+// // MAP
+// const users= [
+//     {id:1, name: 'John'}
+//     {id:2, name: 'Sara'}
+//     {id:3, name: 'Karen'}
+// ]
+
+// const ids = users.map(function(user){
+//     return user.id;
+// })
+
+// console.log(id);
+
+// FOR IN LOOP
+const user = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 40
+}
+
+for(let x in user){
+    console.log(`${x} : ${user[x]}`);
+}
